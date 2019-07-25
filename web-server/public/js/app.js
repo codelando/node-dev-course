ed204f54762b134fe067dd88fcdfa47ddbb1761f
Ezequiel Cortés <codelando@gmail.com>
2019-07-24 23:29:46 -0300

    Web server: forecast search form, minor corrections

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,24 @@
-console.log('Client side JavaScript loaded successfuly');
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
+
+weatherForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const location = search.value;
+
+    messageOne.textContent = 'Fetching weather data...';
+    messageTwo.textContent = '';
+
+    fetch(`/weather?address=${location}`).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error;
+            } else {
+                console.log(data.forecast);
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast;
+            }
+        }); 
+    });
+});
